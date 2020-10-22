beda721ec3e287ba31a905a6bc3900b37cbfe243
Patrick Bradshaw <patrick.bradshaw@parivedasolutions.com>
2020-10-22 16:21:39 -0500

    Jsdoc with betterdocs + initial documentation

--- client/aws/simpleCredentialProvider.js
+++ client/aws/simpleCredentialProvider.js
@@ -1,12 +1,29 @@
 const { AWSOsduBaseCredentialProvider } = require('./baseCredentialProvider');
 
+/**
+ * Simple credenial provider class that provides Cognito username and password from memory
+ * @class
+ * @category Clients
+ * @subcategory AWS
+ */
 class AWSOsduSimpleCredentialProvider extends AWSOsduBaseCredentialProvider {
+    /**
+     * @constructor
+     * @param {string} username - Cognito username
+     * @param {string} password - Cognito password
+     */
     constructor(username, password) {
         super();
         this._username = username;
         this._password = password;
     }
 
+    /**
+     * Serve credentials from memory
+     * @returns {Promise<Object>} credentials - Promise resolving with Cognito username and password
+     * @returns {string} credentials.username - Cognito username
+     * @returns {string} credentials.password - Cognito password
+     */
     async GetCredentials() {
         return {
             username: this._username,
--- client/aws/ssmCredentialProvider.js
+++ client/aws/ssmCredentialProvider.js
@@ -1,7 +1,20 @@
 const { AWSOsduBaseCredentialProvider } = require('./baseCredentialProvider');
 const AWS = require('aws-sdk');
 
+/**
+ * Credenial provider class that provides Cognito username and password from AWS SSM parameters
+ * @class
+ * @category Clients
+ * @subcategory AWS
+ */
 class AWSOsduSSMCredentialProvider extends AWSOsduBaseCredentialProvider {
+    /**
+     * @param {Object} params - AWS configuration and SSM parameters
+     * @param {string} params.aws_region - The AWS region in which the OSDU application is deployed
+     * @param {string} [params.profile] - The AWS credential profile to use when making requests to SSM (typically only for local development)
+     * @param {string} params.username_parameter - SSM parameter identifier where the Cognito username is stored
+     * @param {string} params.password_parameter - SSM parameter identifier where the Cognito password is stored
+     */
     constructor(params) {
         super();
         this._usernameParameter = params.username_parameter;
@@ -10,6 +23,14 @@ class AWSOsduSSMCredentialProvider extends AWSOsduBaseCredentialProvider {
         this._profile = params.aws_profile;
     }
 
+    /**
+     * Fetch credentials from SSM and serve dynamically
+     * - Configures AWS SDK to use provided credentials for local development or automatic credentials for hosted applications
+     * - Brokers communication with AWS SSM to retrieve Username/Password parameters
+     * @returns {Promise<Object>} credentials - Promise resolving with Cognito username and password
+     * @returns {string} credentials.username - Cognito username
+     * @returns {string} credentials.password - Cognito password
+     */
     GetCredentials() {
         if (!this._ssm) {
             var config = {
